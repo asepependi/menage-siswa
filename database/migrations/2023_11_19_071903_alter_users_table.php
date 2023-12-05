a6f301ed70f008dce477395df24d0d9dd2bf20ed
asefe019 <asefe019@gmail.com>
2023-12-05 09:19:58 +0700

    first commits

---
+++ database/migrations/2023_11_19_071903_alter_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username',50)->unique();
+            $table->string('alamat',50)->nullable();
+            $table->string('no_telepon',14);
+            $table->date('tgl_lahir')->nullable();
+            $table->string('avatar');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
